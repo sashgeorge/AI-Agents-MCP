f424755dbd1d537f73eec92ed8dbf3e6a00c9268
Sash Kavalaparambil <sash_george@hotmail.com>
2025-04-21 13:12:17 -0500

    Add files via upload
    Removed unused sections

--- MCPClient.py
+++ MCPClient.py
@@ -78,7 +78,7 @@ class MCPClient:
         response = await self.session.call_tool(tool_name, input_schema)
         print(f"{YELLOW}\nResponse from tool:{RESET} {response}")
 
-        #call "query_default_agent" tool with query "What is the weather today?" connect_agent
+        #call "query_default_agent" tool with query 
         query = "How to reset router?"
         tool_name = "query_default_agent"
         input_schema = {
@@ -87,7 +87,7 @@ class MCPClient:
         response = await self.session.call_tool(tool_name, input_schema)
         print(f"{YELLOW}\nResponse from tool:{RESET} {response}")
 
-        #Call "connect_agent" tool with agent_id "my-agent" and query "What is the weather today?"
+        #Call "connect_agent" tool with agent_id  and query 
         query = "calculate 9+(6x7)/5-1"
         agent_id = "asst_hmSnWbQpaNJVTMxzXu0e8m2m"
         tool_name = "connect_agent" 
@@ -100,99 +100,6 @@ class MCPClient:
 
 
 
-    async def process_query(self, query: str) -> str:
-        """Process a query using Claude and available tools"""
-        system_prompt = "You are a helpful assistant. Answer the user's questions and use tools when necessary."
-        assistant_id = "asst_vG57gEgrt2e6snNI44abAuYK"
-
-        # messages = [
-        #     {"role": "system", "content": prompt},
-        #     {"role": "user", "content": query}
-        # ]
-
-        response = await self.session.list_tools()
-        available_tools = [{ 
-            "name": tool.name,
-            "description": tool.description,
-            "input_schema": tool.inputSchema
-        } for tool in response.tools]
-
-        print("\nAvailable tools:", [tool["name"] for tool in available_tools])
-
-        # Initial Claude API call
-        # response = self.aoai.messages.create(
-        #     model="claude-3-5-sonnet-20241022",
-        #     max_tokens=1000,
-        #     messages=messages,
-        #     tools=available_tools
-        # )
-        # Create a thread
-        thread = self.client.beta.threads.create()
-
-        # Add a user question to the thread
-        message = self.client.beta.threads.messages.create(
-        thread_id=thread.id,
-        role="user",
-        content=query # Replace this with your prompt
-        )
-
-
-        # assistant = self.client.beta.assistants.update(
-        # id=assistant_id,              # Use the unique identifier from the created assistant
-        # instructions=system_prompt)
-
-
-        # Run the thread
-        run = self.client.beta.threads.runs.create(
-        thread_id=thread.id,
-        assistant_id=assistant_id
-        )
-
-        # Looping until the run completes or fails
-        while run.status in ['queued', 'in_progress', 'cancelling']:
-            time.sleep(1)  # Wait for a second before checking again
-            run = self.client.beta.threads.runs.retrieve(
-                thread_id=thread.id,
-                run_id=run.id
-            )
-
-            if run.status == 'completed':
-                messages = self.client.beta.threads.messages.list(
-                    thread_id=thread.id
-                )
-                print(messages)
-            elif run.status == 'requires_action':
-                # the assistant requires calling some functions
-                # and submit the tool outputs back to the run
-                pass
-            else:
-                print(run.status)
-
-        # final_text.append(response.content[0].text)
-
-        return "\n".join(messages)
-
-    async def chat_loop(self):
-        """Run an interactive chat loop"""
-        print("\nMCP Client Started!")
-        print("Type your queries or 'quit' to exit.")
-        
-        while True:
-            try:
-                query = input("\nQuery: ").strip()
-                
-                if query.lower() == 'quit':
-                    break
-                    
-                response = await self.process_query(query)
-                print("\n" + response)
-                    
-            except Exception as e:
-                print(f"\nError: {str(e)}")
-    
-    async def cleanup(self):
-        """Clean up resources"""
-        await self.exit_stack.aclose()
 
 async def main():
     # if len(sys.argv) < 2:
@@ -203,7 +110,7 @@ async def main():
     client = MCPClient()
     try:
         # await client.connect_to_server(sys.argv[1])
-        await client.connect_to_server("TPD_AzureAIAgent_MCPServer.py")
+        await client.connect_to_server("MCPServer.py")
         # await client.chat_loop()
     finally:
         await client.cleanup()
